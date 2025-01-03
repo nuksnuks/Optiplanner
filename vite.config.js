d494c28ee1f975e7ba24a73ed1da027402a6050f
David Wogelius <davidwogelius@gmail.com>
2025-01-03 01:43:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env,
-  },
+    'process.env': {
+      VITE_API_KEY: JSON.stringify(process.env.VITE_API_KEY),
+      VITE_AUTH_DOMAIN: JSON.stringify(process.env.VITE_AUTH_DOMAIN),
+      VITE_DATABASE_URL: JSON.stringify(process.env.VITE_DATABASE_URL),
+      VITE_PROJECT_ID: JSON.stringify(process.env.VITE_PROJECT_ID),
+      VITE_STORAGE_BUCKET: JSON.stringify(process.env.VITE_STORAGE_BUCKET),
+      VITE_MESSAGING_SENDER_ID: JSON.stringify(process.env.VITE_MESSAGING_SENDER_ID),
+      VITE_APP_ID: JSON.stringify(process.env.VITE_APP_ID),
+      VITE_MEASUREMENT_ID: JSON.stringify(process.env.VITE_MEASUREMENT_ID),
+    }
+  }
 });
